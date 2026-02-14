b6cf145036dfd85a352efb074d9ac362b54ac3c5
gretmn102 <treesortbubble@gmail.com>
2026-02-14 18:14:06 +0200

    feat: add init achievement

--- scripts.fsx
+++ scripts.fsx
@@ -9,6 +9,9 @@ open FsharpMyExtension.Containers
 
 let (</>) x y = Path.Combine(x, y)
 
+let gamePath = "src" </> "game.twee"
+let newlineType = Twee.FSharp.NewlineType.Lf
+
 module ImageMagick =
   let convert src dst =
     let convertPath =
@@ -93,24 +96,28 @@ let useImageInTopPassage passageName (styleRule: ImageCssRule) twee =
     }
   )
 
-let addImage imageName passageName =
-  let gamePath = "src" </> "game.twee"
+let updateTwee update =
   Result.builder {
+    let! twee = Twee.FSharp.Document.parseFile gamePath
+    let twee = update twee
+    let rawTwee = Twee.FSharp.Document.toString newlineType twee
+    let rawTwee = $"{rawTwee}{Twee.FSharp.NewlineType.toString newlineType}"
+    IO.File.WriteAllText(gamePath, rawTwee)
+    return ()
+  }
+
+let addImage imageName passageName =
+  updateTwee (fun twee ->
     let imagePath = @"src" </> "images" </> imageName + ".webp"
     let imageCssRule = ImageCssRule.createFromFile imagePath
-    let! twee = Twee.FSharp.Document.parseFile gamePath
     let twee = appendStyleRuleToStylesheet imageCssRule twee
     let twee =
       twee
       |> useImageInTopPassage
         (fun passage -> passage.Header.Name = passageName)
         imageCssRule
-    let newlineType = Twee.FSharp.NewlineType.Lf
-    let rawTwee = Twee.FSharp.Document.toString newlineType twee
-    let rawTwee = $"{rawTwee}{Twee.FSharp.NewlineType.toString newlineType}"
-    IO.File.WriteAllText(gamePath, rawTwee)
-    return ()
-  }
+    twee
+  )
   |> printfn "%A"
 
 // ImageMagick.convertFolderToWebp false "src/images"
@@ -132,18 +139,130 @@ let addImages () =
   addImage "1769882666" "Присмотреться"
   addImage "bucket-with-potatoes" "Продрать очи" // 1769706858
 
-ImageMagick.convertFolderToWebp false "src/achievement-images"
 
-let addAchievements () =
-  let create achievementName passageName achievementGitHubImagePath achievementImagePath =
-    failwith "todo"
-  [
-    create "Есть контакт"      "Сдаться чарам девушки"        "151ea21b-d705-4e94-aef4-01cafd683c1f" "549112654-151ea21b-d705-4e94-aef4-01cafd683c1f"
-    create "Крушитель"         "Выломать дверь"               "f506f819-c35e-45c9-a6df-e2dcf330f11a" "549112688-f506f819-c35e-45c9-a6df-e2dcf330f11a"
-    create "Спасатель курочки" "Отпустить курицу"             "17991d3a-b2d2-4ec5-9c86-0986a06b9a49" "549112727-17991d3a-b2d2-4ec5-9c86-0986a06b9a49"
-    create "Первая кровь"      "Выпить кровь курицы"          "31a415d8-434f-4ab7-9094-1afac56a5595" "549112826-31a415d8-434f-4ab7-9094-1afac56a5595"
-    create "Спасатель зайчика" "Освободить зайчика и убежать" "cbcac3f6-2d65-4d43-9589-22dd71c6e35b" "549112883-cbcac3f6-2d65-4d43-9589-22dd71c6e35b"
-    create "Исцеление"         "Концовка исцеление"           "287c9124-4b44-4048-8498-fe3cc527294d" "549112925-287c9124-4b44-4048-8498-fe3cc527294d"
-    create "Монстр"            "Концовка монстр"              "d1b89dc9-f03f-4200-8822-6d3ffda7116e" "549112983-d1b89dc9-f03f-4200-8822-6d3ffda7116e"
-    create "Псих"              "Концовка псих"                "5d2042c6-a526-40d2-9f8b-16ed9e620cf2" "549113060-5d2042c6-a526-40d2-9f8b-16ed9e620cf2"
-  ]
+module Achievements =
+  open FsharpMyExtension.Serialization.Serializers.ShowList
+
+  type Achievement = {
+    Name: string
+    Description: string option
+    GitHubImagePath: string
+    ImagePath: string
+    PassageName: string
+  }
+
+  [<RequireQualifiedAccess>]
+  [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+  module Achievement =
+    let create achievementName passageName achievementGitHubImagePath achievementImagePath : Achievement =
+      {
+        Name = achievementName
+        PassageName = passageName
+        GitHubImagePath = achievementGitHubImagePath
+        ImagePath = achievementImagePath
+        Description = None
+      }
+
+  let showTab = showString "  "
+
+  let showField key value =
+    key << showChar ':' << showSpace << value << showChar ','
+
+  let showQuotes =
+    between (showChar '"') (showChar '"')
+
+  let showBlock shows : ShowS list =
+    shows
+    |> List.map (fun (s: ShowS) ->
+      showTab << s
+    )
+
+  let createDefaultAchievements (achievements: Achievement list) =
+    let createField achievementName =
+      showField
+        (showQuotes (showString achievementName))
+        (showString "true")
+    [
+      showString "const defaultAchievements = () => ({"
+      yield!
+        achievements
+        |> List.map (fun achievement ->
+          showTab << createField achievement.Name
+        )
+      showString "})"
+    ]
+
+  let createAchievementDescriptions (achievements: Achievement list) =
+    let createField { Name = name; Description = description} =
+      [
+        showQuotes (showString name) << showString ": {"
+        yield!
+          [
+            showField (showString "name") (showQuotes (showString name))
+            showField (showString "desc") (showQuotes (showString (Option.defaultValue "" description)))
+          ]
+          |> showBlock
+        showString "},"
+      ]
+    [
+      showString "setup.achievementDescriptions = {"
+      yield!
+        achievements
+        |> List.collect (fun achievementName ->
+          createField achievementName
+        )
+        |> showBlock
+      showString "}"
+    ]
+
+  let addInit achievements (twee: Twee.FSharp.Document) =
+    twee
+    |> Twee.FSharp.Document.updatePassage
+      (fun passage ->
+        passage.Header.Name = "StoryInit"
+      )
+      (fun passage ->
+        { passage with
+            Body =
+              passage.Body
+              |> List.collect (fun line ->
+                if not <| line.Contains "// todo: добавить инициализацию достижений" then
+                  [line]
+                else
+                  [
+                    yield! createDefaultAchievements achievements |> showBlock |> List.map show
+                    ""
+                    yield! createAchievementDescriptions achievements |> showBlock |> List.map show
+                  ]
+              )
+        }
+      )
+
+  let createUseTag { Name = achievementName } =
+    String.concat "\n" [
+      $"<<run setup.unlock(\"{achievementName}\")>>\\"
+      $"Вы получили достижение <<link \"{achievementName}\">>"
+      $"  <<run setup.showAchievement(\"{achievementName}\")>>"
+      "<</link>>!"
+    ]
+
+  let achievements =
+    [
+      Achievement.create "Есть контакт"      "Сдаться чарам девушки"        "151ea21b-d705-4e94-aef4-01cafd683c1f" "549112654-151ea21b-d705-4e94-aef4-01cafd683c1f"
+      Achievement.create "Крушитель"         "Выломать дверь"               "f506f819-c35e-45c9-a6df-e2dcf330f11a" "549112688-f506f819-c35e-45c9-a6df-e2dcf330f11a"
+      Achievement.create "Спасатель курочки" "Отпустить курицу"             "17991d3a-b2d2-4ec5-9c86-0986a06b9a49" "549112727-17991d3a-b2d2-4ec5-9c86-0986a06b9a49"
+      Achievement.create "Первая кровь"      "Выпить кровь курицы"          "31a415d8-434f-4ab7-9094-1afac56a5595" "549112826-31a415d8-434f-4ab7-9094-1afac56a5595"
+      Achievement.create "Спасатель зайчика" "Освободить зайчика и убежать" "cbcac3f6-2d65-4d43-9589-22dd71c6e35b" "549112883-cbcac3f6-2d65-4d43-9589-22dd71c6e35b"
+      Achievement.create "Исцеление"         "Концовка исцеление"           "287c9124-4b44-4048-8498-fe3cc527294d" "549112925-287c9124-4b44-4048-8498-fe3cc527294d"
+      Achievement.create "Монстр"            "Концовка монстр"              "d1b89dc9-f03f-4200-8822-6d3ffda7116e" "549112983-d1b89dc9-f03f-4200-8822-6d3ffda7116e"
+      Achievement.create "Псих"              "Концовка псих"                "5d2042c6-a526-40d2-9f8b-16ed9e620cf2" "549113060-5d2042c6-a526-40d2-9f8b-16ed9e620cf2"
+    ]
+
+  // do
+  //   updateTwee (fun twee ->
+  //     addInit achievements twee
+  //   )
+  //   |> printfn "%A"
+
+
+  // ImageMagick.convertFolderToWebp false "src/achievement-images"
