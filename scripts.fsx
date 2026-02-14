89f39036becee2df89ffd2aa495c79b2a1821ad8
Fering <treesortbubble@gmail.com>
2026-02-14 14:19:05 +0200

    feat: add images (#4)
    * add image to спуститься за старушкой
    
    * feat: add `scripts`
    
    * добавить пару картинок
    
    * feat: add images
    
    * загнать картинку "1769801961" в стиль
    
    * загнать картинку "1769882666" в стиль
    
    * загнать картинку "bucket-with-potatoes" в стиль
    
    * refactor(scripts.fsx)
    
    * refactor(scripts.fsx): to `ImageMagick` module
    
    * feat(scripts): add linux path to `convert`
    
    * script: add `TweeApi`
    
    * script: update
    
    * feat(scripts): update
    
    * feat: add Сбежать от мужичка
    
    * wip: pentagram to style
    
    * feat: add Искать бабулю
    
    * feat: add other images
    
    * feat(scripts): update
    
    * chore(editorconfig): set end of line to LF
    
    Closes: #2

---
+++ scripts.fsx
@@ -0,0 +1,134 @@
+#!/usr/bin/env -S dotnet fsi
+#r "nuget: FSharpMyExt, 2.0.0-prerelease.9"
+#r "nuget: Twee.FSharp, 0.2.2"
+open System
+open System.IO
+open FsharpMyExtension
+open FsharpMyExtension.IO
+open FsharpMyExtension.Containers
+
+let (</>) x y = Path.Combine(x, y)
+
+module ImageMagick =
+  let convert src dst =
+    let convertPath =
+      match Environment.OSVersion.Platform with
+      | PlatformID.Unix -> "convert"
+      | _ -> @"e:\msys64\mingw64\bin\convert.exe"
+    Proc.startProcSimple convertPath (
+      [$"\"{src}\""; $"\"{dst}\""]
+      |> String.concat " "
+    )
+
+  let convertFolderToWebp dry dirPath =
+    let dir = DirectoryInfo dirPath
+    let files = dir.GetFiles "*.png"
+    files
+    |> Array.iter (fun file ->
+      let srcPath = file.FullName
+      let dstPath =
+        file.DirectoryName </> $"{Path.GetFileNameWithoutExtension srcPath}.webp"
+      printfn "convert %s %s" srcPath dstPath
+      if not dry then
+        convert srcPath dstPath
+        |> printfn "%d"
+    )
+
+type ImageCssRule = {
+  Name: string
+  Body: string
+}
+[<RequireQualifiedAccess>]
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module ImageCssRule =
+  let createFromFile path =
+    let imageBase64 =
+      File.ReadAllBytes path
+      |> System.Convert.ToBase64String
+    let filename = Path.GetFileNameWithoutExtension path
+    let name = $"img-{filename}"
+    let body =
+      String.concat "\n" [
+        $".{name} {{"
+        "  width: 155px;"
+        "  height: 277px;"
+        $"  background-image: url(\"data:image/webp;base64,{imageBase64}\");"
+        "  background-size: contain;"
+        "  background-repeat: no-repeat;"
+        "  display: inline-block;"
+        "}"
+      ]
+    {
+      Name = name
+      Body = body
+    }
+
+  let createUseHtmlTag (imageStyle: ImageCssRule) =
+    $"<div class=\"{imageStyle.Name} float-right\" />"
+
+let appendStyleRuleToStylesheet (styleRule: ImageCssRule) (twee: Twee.FSharp.Document) =
+  twee
+  |> Twee.FSharp.Document.updatePassage
+    (fun passage ->
+      let header = passage.Header
+      match header.Tags with
+      | None -> false
+      | Some tags ->
+        header.Name = "StoryStylesheet" && Set.contains "stylesheet" tags
+    )
+    (fun passage ->
+      { passage with
+          Body =
+            List.append passage.Body [styleRule.Body]
+      }
+    )
+
+let useImageInTopPassage passageName (styleRule: ImageCssRule) twee =
+  twee
+  |> Twee.FSharp.Document.updatePassage passageName (fun passage ->
+    { passage with
+        Body =
+          let htmlTag = ImageCssRule.createUseHtmlTag styleRule
+          $"{htmlTag}\\"::passage.Body
+    }
+  )
+
+let addImage imageName passageName =
+  let gamePath = "src" </> "game.twee"
+  Result.builder {
+    let imagePath = @"src" </> "images" </> imageName + ".webp"
+    let imageCssRule = ImageCssRule.createFromFile imagePath
+    let! twee = Twee.FSharp.Document.parseFile gamePath
+    let twee = appendStyleRuleToStylesheet imageCssRule twee
+    let twee =
+      twee
+      |> useImageInTopPassage
+        (fun passage -> passage.Header.Name = passageName)
+        imageCssRule
+    let newlineType = Twee.FSharp.NewlineType.Lf
+    let rawTwee = Twee.FSharp.Document.toString newlineType twee
+    let rawTwee = $"{rawTwee}{Twee.FSharp.NewlineType.toString newlineType}"
+    IO.File.WriteAllText(gamePath, rawTwee)
+    return ()
+  }
+  |> printfn "%A"
+
+// ImageMagick.convertFolderToWebp false "src/images"
+
+let addImages () =
+  addImage "pentagram" "Спуститься за старушкой" // 1769298905
+  addImage "1769360965" "Сбежать от мужичка"
+  addImage "1769456633" "Искать бабулю"
+  addImage "1769477722" "Наблюдать за бабулей"
+  addImage "1769547712" "Отправиться в лес"
+  addImage "1769555303" "Пойти за мужичком"
+  addImage "1769626842" "Посмотреть, что там"
+  addImage "1769627225" "Принести дров"
+  addImage "1769709272" "Найти церковь"
+  addImage "1769724226" "Сесть на ближайшую могилу"
+  addImage "1769793840" "Дождаться ночи"
+  addImage "1769797475" "Отправиться домой"
+  addImage "1769801961" "Войти внутрь дома и Зайти"
+  addImage "1769882666" "Присмотреться"
+  addImage "bucket-with-potatoes" "Продрать очи" // 1769706858
+
